991ed9ae0d10c5289a22660f1144d98ff5355850
DaniellaMarchevska05 <daniellamarchevsk@gmail.com>
2024-12-28 23:37:36 +0200

    update4 currently with estimators

--- tensorFlow.py
+++ tensorFlow.py
@@ -6,9 +6,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Set to '1' for warnings, '2' for err
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-#from IPython.display import clear_output
+from IPython.display import clear_output
 from six.moves import urllib
-
 import tensorflow.compat.v2.feature_column as fc
 
 import tensorflow as tf
@@ -63,7 +62,8 @@ column_1_in_row_2_and_3 = tensor[1:3, 0]
 # print(column_1_in_row_2_and_3)
 
 #----------------------------------------------------
-# Load dataset.
+# Load dataset
+ # Load dataset.
 dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data
 dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
 y_train = dftrain.pop('survived')# cutting column survived from dftrain and creating y_train with just this column
@@ -83,7 +83,7 @@ y_eval = dfeval.pop('survived')
 #dftrain['class'].value_counts().plot(kind='barh')
 
 pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive')
-plt.show()
+#plt.show()
 
 
 
@@ -93,11 +93,11 @@ NUMERIC_COLUMNS = ['age', 'fare']
 
 feature_columns = []
 for feature_name in CATEGORICAL_COLUMNS:
-  vocabulary = dftrain[feature_name].unique()  # gets a list of all unique values from given feature column
-  feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+     vocabulary = dftrain[feature_name].unique()  # gets a list of all unique values from given feature column
+     feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
 
 for feature_name in NUMERIC_COLUMNS:
-  feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+    feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
 
 print(feature_columns)
 
@@ -113,3 +113,20 @@ def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32)
 
 train_input_fn = make_input_fn(dftrain, y_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
 eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+train_input_fn = make_input_fn(dftrain, y_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+# We create a linear estimtor by passing the feature columns we created earlier
+
+linear_est.train(train_input_fn)  # train
+result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on tetsing data
+
+clear_output()  # clears consoke output
+print(result['accuracy'])  # the result variable is simply a dict of stats about our model
+
+pred_dicts = list(linear_est.predict(eval_input_fn))
+probs = pd.Series([pred['probabilities'][1] for pred in pred_dicts])
+
+probs.plot(kind='hist', bins=20, title='predicted probabilities')
+plt.show()
